32d798ee41089de85305be1fae8f8282f886f525
Witsarut Sawetkanit <illumina-ova@live.com>
2018-06-16 11:02:46 +0700

    Clean package.js

--- package.js
+++ package.js
@@ -14,28 +14,10 @@ Package.on_use(function (api) {
 
   api.use('yogiben:admin@1.1.0', {weak: true});
 
-  
-
   api.add_files('globals.js', ['client', 'server']);
   api.add_files('illuminist_i18n_db-common.js', ['client', 'server']);
   api.add_files('illuminist_i18n_db-server.js', 'server');
   api.add_files('illuminist_i18n_db-client.js', 'client');
 
   api.export('i18nCollection', ['client', 'server']);
-});
-Package.on_test(function (api) {
-  //
-  // DO NOT ADD THIS Package.on_test TO GIT!!!
-  // DO NOT ADD THIS Package.on_test TO GIT!!!
-  // DO NOT ADD THIS Package.on_test TO GIT!!!
-  //
-  api.use(['ejson', 'underscore', "deps", 'tinytest', 'test-helpers', 'templating', 'jquery', "reactive-dict"], ['client', 'server']);
-  api.use(['session'], ['client']);
-  
-
-  api.use('illuminist:i18n-db', ['client', 'server']);
-
-  api.add_files('unittest/tests/helpers.js', ['client', 'server']);
-  api.add_files('unittest/tests/common.js', ['client', 'server']);
-api.add_files('unittest/tests/enabled.js', ['client', 'server']);
 });
